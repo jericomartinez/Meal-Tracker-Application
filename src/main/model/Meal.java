5bf70c455cf43eabe8ab857c9d29ed5cc79cfe39
UBC Student <jerico@students.cs.ubc.ca>
2024-10-03 22:51:33 -0700

    Added specification to Meal class and created Macronutrients class

--- src/main/model/Meal.java
+++ src/main/model/Meal.java
@@ -9,7 +9,32 @@ public class Meal {
     private int calories;                     // number of calories
     private ArrayList<String> macronutrients; // list of macronutrients
 
-    public Meal() {
-
+    // REQUIRES: name.length() > 0, calories > 0, macronutrients != null
+    // EFFECTS:  creates a meal with this.name is set to name; this.calories 
+    //           is set to calories; this.macronutrients is set to macronutrients.
+    public Meal(String name, int calories, ArrayList<String> macronutrients) {
+        // stub
     }
+
+    public String getName() {
+        return "";
+    }
+
+    public int getCalories() {
+        return 0;
+    }
+
+    public ArrayList<String> getMacronutrients() {
+        return null;
+    }
+
+    public void setName(String name) {
+        // stub
+    }
+
+    public void setCalories(int calories) {
+        // stub
+    }
+
+    // public void getMacronutrients
 }
