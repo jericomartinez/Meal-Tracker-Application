3561880d2dbbdeec6b542911734364299fc8de68
UBC Student <jerico@students.cs.ubc.ca>
2024-10-06 03:06:04 -0700

    changed CalorieTracker to MealTracker

---
+++ src/main/model/MealTracker.java
@@ -0,0 +1,9 @@
+package model;
+// represents a list of meals where you can view the meals you've eaten, select
+// a meal and edit the details of it, set a calorie goal, delete a meal, and view
+// a summary of your day displaying total calories and macronutrients consumed
+public class MealTracker {
+
+    public MealTracker() {
+    }
+}
