4debeabfdc2b1bf150b06075fbfc19470e317392
UBC Student <jerico@students.cs.ubc.ca>
2024-10-06 03:04:24 -0700

    Added tests for Macronutrient

---
+++ src/test/model/TestMacronutrient.java
@@ -0,0 +1,47 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TestMacronutrient {
+
+    private Macronutrient m;
+
+    @BeforeEach
+    public void runBefore() {
+        m = new Macronutrient("Protein", 15);
+    }
+
+    @Test
+    public void testMacronutrientConstructor() {
+        assertEquals("Protein", m.getMacronutrient());
+        assertEquals(15, m.getAmount());
+    }
+
+    @Test
+    public void testGetMacronutrient() {
+        assertEquals("protein", m.getMacronutrient());
+    }
+
+    @Test
+    public void testGetAmount() {
+        assertEquals(15, m.getAmount());
+    }
+
+    @Test
+    public void testSetMacronutrient() {
+        assertEquals("Protein", m.getMacronutrient());
+        m.setMacronutrient("Fat");
+        assertEquals("Fat", m.getMacronutrient());
+    }
+
+    @Test
+    public void testSetAmount() {
+        assertEquals(15, m.getAmount());
+        m.setAmount(20);
+        assertEquals(20, m.getAmount());
+    }
+
+}
