e707d340ac160c01fd4a0082559413c9b2b36113
UBC Student <jerico@students.cs.ubc.ca>
2024-10-08 12:33:45 -0700

    Deleted CalorieTracker and TestCalorieTracker clasees

--- src/main/ui/CalorieTracker.java
+++
@@ -1,9 +0,0 @@
-package ui;
-// CalorieTracker represents a list of meals where you can 
-public class CalorieTracker {
-    int other;
-
-    public CalorieTracker() {
-        other = 4;
-    }
-}
