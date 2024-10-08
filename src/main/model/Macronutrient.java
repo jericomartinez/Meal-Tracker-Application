7c5fd2e9a08126120e77b83f3cff28cbf35ef4bf
UBC Student <jerico@students.cs.ubc.ca>
2024-10-08 12:38:58 -0700

    Edited Macronutrient specification

--- src/main/model/Macronutrient.java
+++ src/main/model/Macronutrient.java
@@ -1,12 +1,12 @@
 package model;
 
-// represents a macronutrient of either protein, fat, carbs, or fibre with
-// a String saying which macronutrient it is and an int as the 
+// represents a macronutrient of either protein, fat, carbohydrate, or fibre with a String 
+// saying which macronutrient it is and an int as the amount in grams (g) present in the meal
 public class Macronutrient {
     private String macronutrient;
     private int amount;
 
-    // REQUIRES: macronutrient is one of protein, carbohydrate, fat, fibre
+    // REQUIRES: macronutrient is one of: protein, carbohydrate, fat, fibre
     // and int >= 0
     public Macronutrient(String macronutrient, int amount) {
         // stub
