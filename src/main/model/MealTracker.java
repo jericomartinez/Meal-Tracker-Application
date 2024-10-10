1a420f7d384f30620bc238003e3347074aacf80c
UBC Student <jerico@students.cs.ubc.ca>
2024-10-10 14:56:39 -0700

    Implemented MealTracker

--- src/main/model/MealTracker.java
+++ src/main/model/MealTracker.java
@@ -6,39 +6,43 @@ import java.util.ArrayList;
 // a meal and edit the details of it, set a calorie goal, delete a meal, and view
 // a summary of your day displaying total calories and macronutrients consumed
 public class MealTracker {
-    
 
-    // EFFECTS: creates a Meal Tracker that is empty
-    public MealTracker() {
+    int calorieGoal;
+    ArrayList<Meal> meals;
 
+    // EFFECTS: creates a Meal Tracker that is empty with a calorie goal
+    public MealTracker(int calorieGoal) {
+        meals = new ArrayList<>();
+        this.calorieGoal = calorieGoal;
     }
 
     public ArrayList<Meal> getMeals() {
-        return null;
+        return meals;
     }
 
     public int getCalorieGoal() {
-        return 0;
+        return calorieGoal;
     }
 
-    public void setMeals() {
-        // stub
-    }
-
-    public void setCalorieGoal() {
-        // stub
+    public void setCalorieGoal(int newCalorieGoal) {
+        calorieGoal = newCalorieGoal;
     }
 
     // REQUIRES: name.length() > 0
+    // EFFECTS: returns meal with same name
     public Meal selectMeal(String name) {
+        for (Meal next : meals) {
+            if (next.getName() == name) {
+                return next;
+            }
+        }
         return null;
     }
 
-    // REQUIRES: name.length() > 0 && calories > 0 && amountMacronutrient > 0 && 
-    //          (macronutrient == "protein" || "fat" || "carbohydrates" || "fibre")
+
     // MODIFIES: this
-    // EFFECTS:  modifies name, calories, and macronutrients of a meal
-    public void editMeal(String name, int calories, int amountMacronutrient, String macronutrient) {
-        // stub
+    // EFFECTS: adds meal to meals
+    public void addMeal(Meal meal) {
+        this.meals.add(meal);
     }
 }
