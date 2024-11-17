7c256216ea9c562a52a9bde7a5bc2115610273b6
UBC Student <jerico@students.cs.ubc.ca>
2024-11-17 12:14:08 -0800

    Specified ScreenPrinter class

---
+++ src/main/ui/ScreenPrinter.java
@@ -0,0 +1,28 @@
+package ui;
+
+import java.awt.Component;
+
+import javax.swing.JInternalFrame;
+import model.MealTracker;
+
+// Referenced from the AlarmSystem lecture-lab
+// https://github.students.cs.ubc.ca/CPSC210/AlarmSystem.git
+// Represents a screen printer for printing meal names to screen
+public class ScreenPrinter extends JInternalFrame {
+	
+    // creates a ScreenPrinter window which will have the meal names printed onto
+	public ScreenPrinter(Component parent) {
+		// stub
+	}
+	
+    // MODIFIES: this
+    // EFFECTS:  prints log of meals
+	public void printLog(MealTracker mt) {
+        // stub
+	}
+	
+    // EFFECTS: places the position of the window relative to the parents window
+	private void setPosition(Component parent) {
+		// stub
+	}
+}
