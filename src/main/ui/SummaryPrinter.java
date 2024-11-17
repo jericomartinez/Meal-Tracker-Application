082e9124cd27c906f7042c1730a70c6ed1a538f2
UBC Student <jerico@students.cs.ubc.ca>
2024-11-17 12:14:27 -0800

    Specified SummaryPrinter class

---
+++ src/main/ui/SummaryPrinter.java
@@ -0,0 +1,28 @@
+package ui;
+
+import java.awt.Component;
+import javax.swing.JInternalFrame;
+
+import model.MealTracker;
+
+// Referenced from the AlarmSystem lecture-lab
+// https://github.students.cs.ubc.ca/CPSC210/AlarmSystem.git
+// Represents a summary printer for printing the summary to screen
+public class SummaryPrinter extends JInternalFrame {
+	
+    // creates a SummaryPrinter window which will have the summary printed onto
+	public SummaryPrinter(Component parent) {
+		// stub
+	}
+	
+    // MODIFIES: this
+    // EFFECTS:  prints summary
+	public void printSummary(MealTracker mt) {
+        // stub
+	}
+	
+    // EFFECTS: places the position of the window relative to the parents window
+	private void setPosition(Component parent) {
+		// stub
+	}
+}
